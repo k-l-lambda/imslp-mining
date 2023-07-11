9f4665f7b7123d4809f38ba51308005d3343d983
k.l.lambda <k.l.lambda@gmail.com>
2023-07-11 14:13:50 +0800

    prismaClient.ts created.

---
+++ README.md
@@ -0,0 +1,1 @@
+# IMSLP Data Mining
--- tools/test.ts
+++ tools/test.ts
@@ -1,12 +1,27 @@
 
-import { PrismaClient } from '@prisma/client';
-
 import "../env";
+import prisma from "./prismaClient";
 
 
 
-const prisma = new PrismaClient();
-global.prisma = prisma;
+/*prisma.work.findMany({
+	where: {
+		OR: {
+			pdfs: {
+				startsWith: "[{",
+			},
+			audios: {
+				startsWith: "[{",
+			},
+		},
+	},
+}).then(works => console.log(works));*/
+
+
+prisma.$queryRaw`SELECT * FROM Work
+WHERE pdfs like '%"savePath"%' OR audios like '%"savePath"%'
+LIMIT 100
+`.then(x => console.log(x));
 
 
 setTimeout(() => {}, 1e+9);
