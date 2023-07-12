c834a0f3d23f070d5e1a67c262f9c25138ebe12e
k.l.lambda <k.l.lambda@gmail.com>
2023-07-12 14:20:56 +0800

    pianoTranscriber.py: skip exist midi files.

--- pianoTranscriber.py
+++ pianoTranscriber.py
@@ -13,13 +13,21 @@ PTI_NAMES = os.getenv('PTI_NAMES').split(',')
 def main():
 	transcriptor = pti.PianoTranscription(device=os.getenv('TORCH_DEVICE'))
 
-	for root, dirs, files in os.walk("./data"):
-		if root.endswith('spleeter'):
-			sources = [f for f in files if f.replace('.wav', '') in PTI_NAMES]
-			for source in sources:
-				print('Transcribing', root)
-				(audio, _) = pti.load_audio(os.path.join(root, source), sr=pti.sample_rate, mono=True)
-				transcriptor.transcribe(audio, os.path.join(root, source.replace('.wav', '.midi')))
+	try:
+		for root, dirs, files in os.walk("./data"):
+			if root.endswith('spleeter'):
+				waves = [f for f in files if f.endswith('.wav')]
+				sources = [f for f in waves if f.replace('.wav', '') in PTI_NAMES]
+				for source in sources:
+					midi_filename = source.replace('.wav', '.midi')
+					if midi_filename in files:
+						print('Skipped:', root)
+					else:
+						print('Transcribing:', root)
+						(audio, _) = pti.load_audio(os.path.join(root, source), sr=pti.sample_rate, mono=True)
+						transcriptor.transcribe(audio, os.path.join(root, midi_filename))
+	except:
+		print(sys.exc_info())
 
 
 if __name__ == "__main__":
