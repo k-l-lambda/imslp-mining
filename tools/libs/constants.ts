99b5235b555347a626cccaa649dc51979cf19f19
k.l.lambda <k.l.lambda@gmail.com>
2023-07-20 11:00:43 +0800

    scoreVision.ts: filter condition added.

--- tools/libs/constants.ts
+++ tools/libs/constants.ts
@@ -15,7 +15,7 @@ export const TORCH_DEVICE = process.env.TORCH_DEVICE;
 export const PROCESS_PREDICTOR_DIR = process.env.PROCESS_PREDICTOR_DIR;
 export const PROCESS_PREDICTOR_CMD = process.env.PROCESS_PREDICTOR_CMD;
 
-export const SCORE_FILTER_CONDITION = process.env.SCORE_FILTER_CONDITION;
+export const SCORE_FILTER_CONDITION = process.env.SCORE_FILTER_CONDITION as ("single_piano"|"1or2pianos");
 
 
 export const VIEWPORT_UNIT = 8;
--- tools/scoreVision.ts
+++ tools/scoreVision.ts
@@ -53,59 +53,88 @@ const main = async () => {
 
 			console.log(String.fromCodePoint(0x1f3bc), `[${workId}/${file.id}]`, score.title);
 
-			const pageCanvases = await Promise.all(score.pages.map(async page => {
-				const buffer = await loadImage(page.source.url);
-				const pngBuffer = await sharp(buffer).toFormat("png").toBuffer();
-				const image = await skc.loadImage(pngBuffer);
-				const pageCanvas = shootPageCanvas({ page, source: image });
+			// brackets prediction
+			if (!omrState.score?.brackets) {
+				const pageCanvases = await Promise.all(score.pages.map(async page => {
+					const buffer = await loadImage(page.source.url);
+					const pngBuffer = await sharp(buffer).toFormat("png").toBuffer();
+					const image = await skc.loadImage(pngBuffer);
+					const pageCanvas = shootPageCanvas({ page, source: image });
+	
+					return {page, pageCanvas};
+				}));
+	
+				for (const {page, pageCanvas} of pageCanvases) {
+					const areas = page.layout.areas.filter(area => area.staves?.middleRhos?.length);
+					const interval = page.source.interval;
+	
+					const bracketImages = page.systems.map((system, systemIndex) => {
+						const {
+							x,
+							y,
+							staves: { middleRhos, phi1 },
+						} = areas[systemIndex];
+	
+						const topMid = middleRhos[0];
+						const bottomMid = middleRhos[middleRhos.length - 1];
+	
+						const sourceRect = {
+							x: x + phi1 - 4 * interval,
+							y: y + topMid - 4 * interval,
+							width: 8 * interval,
+							height: bottomMid - topMid + 8 * interval,
+						};
+	
+						const canvas = new skc.Canvas(VIEWPORT_UNIT * 8, (sourceRect.height / interval) * VIEWPORT_UNIT);
+	
+						const context = canvas.getContext("2d");
+						context.drawImage(pageCanvas, sourceRect.x, sourceRect.y, sourceRect.width, sourceRect.height, 0, 0, canvas.width, canvas.height);
+	
+						return {
+							system,
+							buffer: canvas.toBufferSync("png"),
+						};
+					});
+					/*bracketImages.forEach((img, i) => {
+						fs.writeFileSync(`./test/sys-${i}.png`, img.buffer);
+					});*/
+	
+					const bracketsRes = await pyClients.predictScoreImages("brackets", { buffers: bracketImages.map(x => x.buffer) });
+	
+					bracketImages.forEach(({ system }, index) => {
+						if (bracketsRes[index]) {
+							system.bracketsAppearance = bracketsRes[index];
+							//console.log("res:", system.bracketsAppearance);
+						}
+					});
+				}
+	
+				score.inferenceStaffLayout();
+				console.log("Score staffLayout:", score.staffLayoutCode);
+	
+				omrState.score.brackets = Date.now();
+				fs.writeFileSync(omrStatePath, YAML.stringify(omrState));
+			}
 
-				return {page, pageCanvas};
-			}));
+			let proceed = true;
+			switch (SCORE_FILTER_CONDITION) {
+			case "single_piano":
+				proceed = ["{,}", "{-}", "{,,}", "{--}"].includes(score.staffLayoutCode);
 
-			// brackets
-			for (const {page, pageCanvas} of pageCanvases) {
-				const areas = page.layout.areas.filter(area => area.staves?.middleRhos?.length);
-				const interval = page.source.interval;
+				break;
+			case "1or2pianos":
+				proceed = [
+					"{,}", "{-}", "{,,}", "{--}",
+					"{,},{,}", "{-},{-}", "{-}{-}", "{,}{,}",
+				].includes(score.staffLayoutCode);
 
-				const bracketImages = page.systems.map((system, systemIndex) => {
-					const {
-						x,
-						y,
-						staves: { middleRhos, phi1 },
-					} = areas[systemIndex];
+				break;
+			}
 
-					const topMid = middleRhos[0];
-					const bottomMid = middleRhos[middleRhos.length - 1];
-
-					const sourceRect = {
-						x: x + phi1 - 4 * interval,
-						y: y + topMid - 4 * interval,
-						width: 8 * interval,
-						height: bottomMid - topMid + 8 * interval,
-					};
-
-					const canvas = new skc.Canvas(VIEWPORT_UNIT * 8, (sourceRect.height / interval) * VIEWPORT_UNIT);
-
-					const context = canvas.getContext("2d");
-					context.drawImage(pageCanvas, sourceRect.x, sourceRect.y, sourceRect.width, sourceRect.height, 0, 0, canvas.width, canvas.height);
-
-					return {
-						system,
-						buffer: canvas.toBufferSync("png"),
-					};
-				});
-				/*bracketImages.forEach((img, i) => {
-					fs.writeFileSync(`./test/sys-${i}.png`, img.buffer);
-				});*/
-
-				const bracketsRes = await pyClients.predictScoreImages("brackets", { buffers: bracketImages.map(x => x.buffer) });
-
-				bracketImages.forEach(({ system }, index) => {
-					if (bracketsRes[index]) {
-						system.bracketsAppearance = bracketsRes[index];
-						//console.log("res:", system.bracketsAppearance);
-					}
-				});
+			if (!proceed) {
+				fs.writeFileSync(scorePath, JSON.stringify(score));
+				console.log("Not expected staff layout, skip:", score.staffLayoutCode);
+				continue;
 			}
 
 			++n_score;
@@ -115,9 +144,9 @@ const main = async () => {
 				zeroScores = false;
 			}
 
-			break;	// temp
+			//break;	// temp
 		}
-		break;	// temp
+		//break;	// temp
 	}
 
 	console.log("All works done,", n_work, "works, ", n_score, "scores.");
