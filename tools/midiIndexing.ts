a8010c03d8ae78e76b363f65238a546d5064c8b0
k.l.lambda <k.l.lambda@gmail.com>
2023-07-25 11:09:17 +0800

    tools/midiIndexing.ts created.

---
+++ tools/midiIndexing.ts
@@ -0,0 +1,90 @@
+
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
+import { MIDI } from "@k-l-lambda/music-widgets";
+import { Minhash, LshIndex } from "minhash";
+
+import "../env";
+
+import { DATA_DIR } from "./libs/constants";
+import walkDir from "./libs/walkDir";
+import * as midiHash from "./libs/midiHash";
+
+
+
+interface MidiHash {
+	key: string;
+	hash: Minhash;
+};
+
+
+const hashMidiFile = (file: string, root: string): MidiHash => {
+	const buffer = fs.readFileSync(file);
+	const midi = MIDI.parseMidiData(buffer);
+	const words = midiHash.chordsToWords(midiHash.midiToChords(midi));
+
+	const hash = new Minhash();
+	words.forEach(word => hash.update(word));
+
+	return {
+		key: path.relative(root, file),
+		hash,
+	};
+};
+
+
+const main = async () => {
+	const works = walkDir(DATA_DIR, /\/$/);
+	works.sort((d1, d2) => Number(path.basename(d1)) - Number(path.basename(d2)));
+
+	let n_cluster = 0;
+	let n_work = 0;
+
+	for (const work of works) {
+		const workId = path.basename(work);
+		const fileDirs = walkDir(work, /\/$/);
+		const midiFiles = fileDirs.map(dir => walkDir(dir, /\.midi?$/)).flat(1);
+		if (!midiFiles.length)
+			continue;
+
+		console.log(String.fromCodePoint(0x1f3bc), `[${workId}]`, midiFiles.length, "MIDI files.");
+
+		const hashes = midiFiles.map(file => hashMidiFile(file, work));
+		//console.log("hashes:", hashes);
+
+		const index = new LshIndex();
+		hashes.forEach(hash => index.insert(hash.key, hash.hash));
+
+		// clustering
+		const refs = new Set<string>();
+		const clusters = [] as string[][];
+
+		hashes.forEach(hash => {
+			if (refs.has(hash.key))
+				return;
+
+			const cluster = index.query(hash.hash);
+			clusters.push(cluster);
+
+			cluster.forEach(key => refs.add(key));
+		});
+
+		const multiClusters = clusters.filter(cluster => cluster.length > 1);
+		if (multiClusters.length) {
+			console.log("clusters:", multiClusters);
+			n_cluster += multiClusters.length;
+			++n_work;
+		}
+
+		//console.log("clusters:", clusters);
+		const indexPath = path.join(work, "index.yaml");
+		fs.writeFileSync(indexPath, YAML.stringify({
+			midiClusters: clusters,
+		}));
+	}
+
+	console.log("Done,", n_cluster, "clusters found in", n_work, "works.");
+}
+
+main();
