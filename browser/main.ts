b0216e162e92dff9dc69a31cd8749e92603a14e1
k.l.lambda <k.l.lambda@gmail.com>
2023-11-23 18:44:03 +0800

    browser/main.ts created.

--- browser/main.ts
+++ browser/main.ts
@@ -2,11 +2,31 @@
 import express from "express";
 import http from "http";
 import ip from "ip";
+import path from "path";
+import fs from "fs";
 
 import "../env"
 
 
 const app = express();
+
+
+// CORS header
+app.use("*", (req, res, next) => {
+	if (req.headers.origin) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "*");
+		res.setHeader("Access-Control-Allow-Headers", "*");
+	}
+
+	next();
+});
+
+
+console.log("root", fs.realpathSync(__dirname));
+const staticDir = dir => express.static(path.resolve(fs.realpathSync(__dirname), dir));
+
+app.use("/", staticDir("./public"));
 
 
 const httpServer = http.createServer(app);
