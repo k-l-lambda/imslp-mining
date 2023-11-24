6997b984dd95ed8536fceaa9cb954fbdb09df954
k.l.lambda <k.l.lambda@gmail.com>
2023-11-24 13:43:27 +0800

    index.vue: work id list loading added.

--- browser/app/index.vue
+++ browser/app/index.vue
@@ -1,10 +1,29 @@
 <script setup>
+	import { ref, onMounted, watch } from "vue";
 
+
+	const workIdList = ref([]);
+	const workId = ref("");
+
+
+	onMounted(async () => {
+		const response = await fetch("/workid-list");
+		workIdList.value = await response.json();
+		//console.log("workIds:", workIdList.value);
+	});
+
+
+	watch(workId, (value) => {
+		console.log("workId:", value);
+	});
 </script>
 
 <template>
 	<div>
-		index.vue
+		<datalist id="workid-list">
+			<option v-for="id of workIdList" :key="id" :value="id" />
+		</datalist>
+		<input type="text" v-model.lazy="workId" list="workid-list" />
 	</div>
 </template>
 
